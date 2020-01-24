da5a499d290ece373b4f177e41cac0616c558c4f
Maarten Lauwers <>
2020-01-24 22:22:04 +0100

    Add line information

--- src/chunk.h
+++ src/chunk.h
@@ -13,6 +13,7 @@
 #include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -20,12 +21,13 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
     ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk *chunk, uint8_t byte);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 
 #endif //CLOX_CHUNK_H
